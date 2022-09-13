5faeb4afb2b7c1ca36e51a111d06bb99787b662d
Romskul <inaki.martin.c@gmail.com>
2022-09-13 19:20:22 +0200

    Interpolacion con texto dinámico

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -5,9 +5,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './empleado.component.html',
   //template:"<p>Aquí iría el empleado</p>",          *Esta seria la forma de poner el template en linea (inline)
   styleUrls: ['./empleado.component.css']
-  //styles: ["p{background-color:red}"]                 *Esta seria la forma de poner el style en linea (inline)
+  //styles: ["p{background-color:red}"]               *Esta seria la forma de poner el style en linea (inline)
 })
 export class EmpleadoComponent implements OnInit {
+
+  nombre = "Juan";
+  apellido = "Diaz";
+  edad = 30;
+  // private edad = 30;                               *Esta seria la forma de declarar una variable privada sólamente accesible desde este documento.
+  // empresa = "Ticmatic";
+
+  // getEdad(){                                       *Esta seria la forma de crear un método para obtener la edad.                  
+  //   return this.edad;
+  // }
+  llamaEmpresa(value:string){
+
+  }
 
   constructor() { }
 
