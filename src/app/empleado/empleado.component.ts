c724ca7a5985bce5711db2702341c930b32153e5
Romskul <inaki.martin.c@gmail.com>
2022-09-14 12:40:02 +0200

    event bindings

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -20,9 +20,21 @@ export class EmpleadoComponent implements OnInit {
   habilitacionCuadro = false;
   usuRegistrado = false;
 
+  textoDeRegistro = "No hay nadie registrado";
   
   getRegistroUsuario(){
     this.usuRegistrado = false;
+  }
+
+  setUsuarioRegistrado(event:Event){
+
+    // alert ("El usuario se acaba de registrar");
+    // this.textoDeRegistro = "El usuario se acaba de registrar";
+    if ((<HTMLInputElement>event.target).value == "si"){
+      this.textoDeRegistro = "El usuario se acaba de registrar"; 
+    }else {
+      this.textoDeRegistro = "No hay nadie registrado";
+    }
   }
 
   llamaEmpresa(value:string){
